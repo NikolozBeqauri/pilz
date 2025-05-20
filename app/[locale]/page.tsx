06f8c099ecc6a7fd5b53d4617f2f938d7646ab45
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-05-20 18:40:47 +0400

    feat: added navigation mediaqueries

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,22 +1,21 @@
-import Link from 'next/link';
 import AboutSection from '../components/About/AboutSection';
 import BankSlider from '../components/BankSlider/BankSlider';
 import ContactSection from '../components/ContactSection/ContactSection';
 import FooterComponent from '../components/FooterComponent/FooterComponent';
 import Hero from '../components/Hero/Hero';
-import Navigation from '../components/Navigation/Navigation';
+import NavigationRender from '../components/NavigationRender/NavigationRender';
 import Services from '../components/Services/Services';
- 
+
 export default function HomePage() {
   return (
     <div>
-      <Navigation/>
-      <Hero/>
-      <Services/>
-      <AboutSection/>
-      <BankSlider/>
-      <ContactSection/>
-      <FooterComponent/>
+      <NavigationRender />
+      <Hero />
+      <Services />
+      <AboutSection />
+      <BankSlider />
+      <ContactSection />
+      <FooterComponent />
     </div>
   );
 }
