d9164a3c832789beeafa6834da08acc9d82cf851
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-05-25 14:42:36 +0400

    feat: added english version of footer

--- app/components/FooterComponent/FooterComponent.tsx
+++ app/components/FooterComponent/FooterComponent.tsx
@@ -2,8 +2,11 @@ import styles from './FooterComponent.module.scss';
 import Image from 'next/image';
 import SocialLinks from './SocialLinks/SocialLinks';
 import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 
 const FooterComponent = () => {
+        const t = useTranslations('Footer');
+    
     return (
         <footer className={styles.background}>
             <div className={styles.footerWrapper}>
@@ -16,23 +19,23 @@ const FooterComponent = () => {
                             height={38}
                             className={styles.image}
                         />
-                        <p>ფინანსური აპლიკაცია, რომელიც დაგეხმარება დეკლარაციის უსწრაფესად შევსებაში</p>
+                        <p>{t("paragraph")}</p>
                         <SocialLinks />
                     </div>
 
                     <div className={styles.secondDiv}>
                         <nav className={styles.innerNavigation}>
-                            <h4 className={styles.footerTitle}>ნავიგაცია</h4>
+                            <h4 className={styles.footerTitle}>{t("navigationTitle")}</h4>
                             <ul>
-                                <li><Link href="#services" className={styles.footerSecondaryText}>სერვისები</Link></li>
-                                <li><Link href="#aboutUs" className={styles.footerSecondaryText}>ჩვენ შესახებ</Link></li>
-                                <li><Link href="#contact" className={styles.footerSecondaryText}>კონტაქტი</Link></li>
+                                <li><a href="#services" className={styles.footerSecondaryText}>{t("services")}</a></li>
+                                <li><a href="#aboutUs" className={styles.footerSecondaryText}>{t("aboutUs")}</a></li>
+                                <li><a href="#contact" className={styles.footerSecondaryText}>{t("contact")}</a></li>
                             </ul>
                         </nav>
 
 
                         <div className={styles.contactsWrapper}>
-                            <h4 className={styles.footerTitle}>დაგვიკავშირდი</h4>
+                            <h4 className={styles.footerTitle}>{t("contactUsTitle")}</h4>
                             <div className={styles.contacts}>
                                 <div className={styles.contactsItem}>
                                     <Image
@@ -42,7 +45,7 @@ const FooterComponent = () => {
                                         height={20}
                                         className={styles.image}
                                     />
-                                    <p className={styles.footerSecondaryText}>info@gmail.com</p>
+                                    <p className={styles.footerSecondaryText}>{t("gmail")}</p>
                                 </div>
 
                                 <div className={styles.contactsItem}>
@@ -53,7 +56,7 @@ const FooterComponent = () => {
                                         height={20}
                                         className={styles.image}
                                     />
-                                    <p className={styles.footerSecondaryText}>(+995 577 55 55 55)</p>
+                                    <p className={styles.footerSecondaryText}>{t("number")}</p>
                                 </div>
 
                                 <div className={styles.contactsItem}>
@@ -64,14 +67,14 @@ const FooterComponent = () => {
                                         height={20}
                                         className={styles.image}
                                     />
-                                    <p className={styles.footerSecondaryText}>Tbilisi, Georgia</p>
+                                    <p className={styles.footerSecondaryText}>{t("location")}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className={styles.copyright}>
-                    <p>Copyright © 2025 პილცი</p>
+                    <p>{t("copyRight")}</p>
                     <p>All Rights Reserved | <a href="#" className={styles.linkStyle}>Terms and Conditions</a> | <a className={styles.linkStyle} href="#">Privacy Policy</a></p>
                 </div>
             </div>
