f8184611d0ddc60af8bc52fcc5739f577678bf87
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-05-31 12:05:11 +0400

    feat: set ka as default

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,39 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { routing } from './i18n/routing';
 import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
- 
-export default createMiddleware(routing);
- 
+
+export default function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+  const pathnameSegments = url.pathname.split('/');
+  const firstSegment = pathnameSegments[1];
+
+  const localeCookie = req.cookies.get('NEXT_LOCALE')?.value;
+
+  if (routing.locales.includes(firstSegment as typeof routing.locales[number])) {
+    if (firstSegment !== localeCookie) {
+      const res = createMiddleware(routing)(req);
+      res.cookies.set('NEXT_LOCALE', firstSegment, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 365 
+      });
+      return res;
+    }
+
+    return createMiddleware(routing)(req);
+  }
+
+  const newLocale = localeCookie || 'ka';
+  url.pathname = `/${newLocale}${url.pathname}`;
+
+  const res = NextResponse.redirect(url);
+  res.cookies.set('NEXT_LOCALE', newLocale, {
+    path: '/',
+    maxAge: 60 * 60 * 24 * 365 
+  });
+
+  return res;
+}
+
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
